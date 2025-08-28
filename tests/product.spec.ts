b9c67217776b30aa6ad051c5642701b9918d2adc
Lidia Parkhomova <slipinuk@gmail.com>
2025-08-28 22:51:47 +0200

    fix with page object

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -1,3 +1,4 @@
+import { Checkout } from './../pages/checkout.page';
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/home.page';
 import { ProductDetails } from '../pages/productDetails.page';
@@ -5,6 +6,7 @@ import { ProductDetails } from '../pages/productDetails.page';
 test('Verify user can add product to cart', async ({ page }) => {
   const homePage = new HomePage(page);
   const productDetails = new ProductDetails(page);
+  const checkout = new Checkout(page);
 
   await page.goto('/');
   await homePage.openProductCard();
@@ -14,20 +16,14 @@ test('Verify user can add product to cart', async ({ page }) => {
   await expect(productDetails.addToCartButton).toBeVisible();
 
   await productDetails.addToCartButton.click();
-  const alertMessage = page.getByRole('alert', { name: 'Product added to shopping' });
-  const cartQuantity = page.getByTestId('cart-quantity');
-  const cardIcon = page.getByTestId('nav-cart');
-  await expect(alertMessage).toBeVisible();
-  await expect(alertMessage).toContainText('Product added to shopping cart');
-  await expect(alertMessage).toBeHidden({ timeout: 8000 });
-  await expect(cartQuantity).toHaveText('1');
+  await expect(productDetails.alertMessage).toBeVisible();
+  await expect(productDetails.alertMessage).toContainText('Product added to shopping cart');
+  await expect(productDetails.alertMessage).toBeHidden({ timeout: 8000 });
+  await expect(productDetails.cartQuantity).toHaveText('1');
 
-  await cardIcon.click();
-  const productQuantity = page.getByTestId('product-quantity');
-  const productTitle = page.getByTestId('product-title');
-  const btnProceed = page.getByTestId('proceed-1');
+  await productDetails.cardIcon.click();
   await expect(page).toHaveURL('/checkout');
-  await expect(productQuantity).toHaveValue('1');
-  await expect(productTitle).toContainText('Slip Joint Pliers');
-  await expect(btnProceed).toBeVisible();
+  await expect(checkout.productQuantity).toHaveValue('1');
+  await expect(checkout.productTitle).toContainText('Slip Joint Pliers');
+  await expect(checkout.btnProceed).toBeVisible();
 });
