12e602711cd54fe481c1e21e9d4a08e97dea2187
Lidia Parkhomova <slipinuk@gmail.com>
2025-09-10 23:48:40 +0200

    fixed

--- tests/add-product-and-complete-checkout.spec.ts
+++ tests/add-product-and-complete-checkout.spec.ts
@@ -2,14 +2,15 @@ import { expect } from '@playwright/test';
 import { test } from '../fixtures';
 import { parsePrice } from '../utils/parse';
 import { getExpirationPlusMonths } from '../utils/expirationMonth';
+import { USER } from '../credentials';
 
 test('User can add product to cart and complete checkout with credit card', async ({ app }) => {
   
   await app.page.goto('/');
 
   // 1
-  const gridName = (await app.homePage.firstCard.getByTestId('product-name').innerText()).trim();
-  const gridPriceText = (await app.homePage.firstCard.getByTestId('product-price').innerText()).trim();
+  const gridName = (await app.homePage.firstCardName.innerText()).trim();
+  const gridPriceText = (await app.homePage.firstCardPrice.innerText()).trim();
   const gridPrice = parsePrice(gridPriceText);
 
   await app.homePage.firstCard.click();
@@ -23,9 +24,9 @@ test('User can add product to cart and complete checkout with credit card', asyn
   // 2
   await app.productDetails.cardIcon.click();
 
-  const cartTitle = (await app.page.getByTestId('product-title').first().innerText()).trim();
-  const cartUnitPriceText = (await app.page.getByTestId('product-price').first().innerText()).trim();
-  const cartTotalText = (await app.page.getByTestId('line-price').first().innerText()).trim();
+  const cartTitle = (await app.productDetails.productTitle.first().innerText()).trim();
+  const cartUnitPriceText = (await app.productDetails.productPrizes.first().innerText()).trim();
+  const cartTotalText = (await app.productDetails.productLinePrice.first().innerText()).trim();
 
   const cartUnitPrice = parsePrice(cartUnitPriceText);
   const cartTotal = parsePrice(cartTotalText);
@@ -50,7 +51,7 @@ test('User can add product to cart and complete checkout with credit card', asyn
 
   await app.checkout.inputExpData.fill(getExpirationPlusMonths(3));
   await app.checkout.inputCvv.fill('111');
-  await app.checkout.inputCardName.fill('Jane Doe');
+  await app.checkout.inputCardName.fill(USER.name);
   await app.checkout.btnFinish.click();
 //6
   await expect(app.checkout.paymentSuccessMessage).toContainText('Payment was successful');
