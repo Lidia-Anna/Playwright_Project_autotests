3a1effbdab243d3135334b1817e81117bc098c06
Lidia Parkhomova <slipinuk@gmail.com>
2025-09-11 17:31:10 +0200

    fix

--- credentials.ts
+++ credentials.ts
@@ -1,6 +1,6 @@
-import { EMAIL, USER_PASSWORD } from './config/baseConfig';
+import { EMAIL, USER_PASSWORD, USER_NAME } from './config/baseConfig';
 export const USER = {
   email: EMAIL,
   password: USER_PASSWORD,
-  name: 'Jane Doe'
+  name: USER_NAME
 };
