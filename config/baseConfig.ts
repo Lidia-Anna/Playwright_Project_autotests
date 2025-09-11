b00b1bbceef6583d35d8587bb9966884ec5d2eb1
Lidia Parkhomova <slipinuk@gmail.com>
2025-09-11 16:12:23 +0200

    fix

--- config/baseConfig.ts
+++ config/baseConfig.ts
@@ -5,6 +5,6 @@ dotenvConfig({ path: join(process.cwd(), '.env') });
 
 
   export const WEB_URL = process.env.WEB_URL;
-  export const EMAIL = process.env.EMAIL ?? 'customer@practicesoftwaretesting.com';
+  export const EMAIL = process.env.EMAIL;
   export const USER_PASSWORD = process.env.USER_PASSWORD;
   export const USER_NAME= process.env.USER_NAME;
